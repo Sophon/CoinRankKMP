917b6f6b57813b4982be5a8f203cd4ce7cdfafc1
Le Duy Anh <lenduya@gmail.com>
2025-03-17 22:28:24 +0100

    feat (ui): color scheme [and|ios]

--- composeApp/src/commonMain/kotlin/org/example/udemykmp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/App.kt
@@ -1,10 +1,13 @@
 package org.example.udemykmp
 
 import androidx.compose.runtime.Composable
+import org.example.udemykmp.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    //TODO:
+    AppTheme {
+        //content
+    }
 }
