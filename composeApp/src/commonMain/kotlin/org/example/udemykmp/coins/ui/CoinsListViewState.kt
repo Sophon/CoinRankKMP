d648441b3a766649c8405f467ccd3162ce6cd6e8
Le Duy Anh <lenduya@gmail.com>
2025-03-27 04:58:46 +0100

    feat (coins): viewstate [kmp]

---
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/coins/ui/CoinsListViewState.kt
@@ -0,0 +1,19 @@
+package org.example.udemykmp.coins.ui
+
+import androidx.compose.runtime.Stable
+import org.jetbrains.compose.resources.StringResource
+
+@Stable
+data class CoinsListViewState(
+    val error: StringResource? = null,
+    val coins: List<UiCoinsListItem> = emptyList(),
+)
+
+data class UiCoinsListItem(
+    val id: String,
+    val name: String,
+    val iconUrl: String,
+    val formattedPrice: String,
+    val formattedChange: String,
+    val isPositive: Boolean,
+)
