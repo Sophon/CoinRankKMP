0c88a848bfc6cf9810adfe90f106c3a294c09c24
Le Duy Anh <lenduya@gmail.com>
2025-03-27 08:20:59 +0100

    feat (di): declare dependencies in the module [kmp]

--- composeApp/src/commonMain/kotlin/org/example/udemykmp/di/Module.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/di/Module.kt
@@ -1,10 +1,19 @@
 package org.example.udemykmp.di
 
 import io.ktor.client.HttpClient
+import org.example.udemykmp.coins.data.remote.impl.CoinsKtorRemoteDataSource
+import org.example.udemykmp.coins.domain.api.CoinsRemoteDataSource
+import org.example.udemykmp.coins.domain.usecases.GetCoinDetailsUseCase
+import org.example.udemykmp.coins.domain.usecases.GetCoinPriceHistory
+import org.example.udemykmp.coins.domain.usecases.GetCoinsListUseCase
+import org.example.udemykmp.coins.ui.CoinsListViewModel
 import org.example.udemykmp.core.network.HttpClientFactory
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun initKoin(config: KoinAppDeclaration? = null) =
@@ -23,6 +32,15 @@ expect val platformModule: Module
 //shared dependencies
 val sharedModule = module {
 
-    //core
+    //region Core
     single<HttpClient> { HttpClientFactory.create(get()) }
+    //endregion
+
+    //region Coins
+    viewModel { CoinsListViewModel(get()) }
+    singleOf(::CoinsKtorRemoteDataSource).bind<CoinsRemoteDataSource>()
+    singleOf(::GetCoinsListUseCase)
+    singleOf(::GetCoinDetailsUseCase)
+    singleOf(::GetCoinPriceHistory)
+    //endregion
 }
