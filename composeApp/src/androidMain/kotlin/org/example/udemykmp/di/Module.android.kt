ff5e85fdb80dcb31d5b3919d38db55ab8005cc19
Le Duy Anh <lenduya@gmail.com>
2025-03-31 00:15:37 +0200

    feat (portfolio): database on Android [and]

--- composeApp/src/androidMain/kotlin/org/example/udemykmp/di/Module.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/udemykmp/di/Module.android.kt
@@ -1,11 +1,17 @@
 package org.example.udemykmp.di
 
+import androidx.room.RoomDatabase
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.android.Android
+import org.example.udemykmp.core.database.PortfolioDatabase
+import org.example.udemykmp.core.database.getPortfolioDatabaseBuilder
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformModule = module {
 
     //core
     single<HttpClientEngine> { Android.create() }
+    singleOf(::getPortfolioDatabaseBuilder).bind<RoomDatabase.Builder<PortfolioDatabase>>()
 }
