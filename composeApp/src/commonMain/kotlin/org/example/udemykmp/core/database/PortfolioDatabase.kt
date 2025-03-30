c0281d21a85a482987e1611bfd2670b9379ffbdf
Le Duy Anh <lenduya@gmail.com>
2025-03-31 00:30:19 +0200

    chore (core): mark classes for future refactor [kmp]

--- composeApp/src/commonMain/kotlin/org/example/udemykmp/core/database/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/core/database/PortfolioDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.RoomDatabase
 import org.example.udemykmp.features.portfolio.data.local.PortfolioCoinEntity
 import org.example.udemykmp.features.portfolio.data.local.PortfolioDao
 
+/**
+ * TODO: this is only used with feature/portfolio, move it there
+ * same with the Database factory
+ */
 @Database(entities = [PortfolioCoinEntity::class], version = 1)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract val portfolioDao: PortfolioDao
