2f75840872ed46ae000cf35f8f5f67f54f0f57ff
Le Duy Anh <lenduya@gmail.com>
2025-03-31 00:22:27 +0200

    feat (portfolio): database on iOS [ios]

--- composeApp/src/iosMain/kotlin/org/example/udemykmp/di/Module.ios.kt
+++ composeApp/src/iosMain/kotlin/org/example/udemykmp/di/Module.ios.kt
@@ -1,11 +1,17 @@
 package org.example.udemykmp.di
 
+import androidx.room.RoomDatabase
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import org.example.udemykmp.core.database.PortfolioDatabase
+import org.example.udemykmp.core.database.getPortfolioDatabaseBuilder
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformModule = module {
 
     //core
     single<HttpClientEngine> { Darwin.create() }
+    singleOf(::getPortfolioDatabaseBuilder).bind<RoomDatabase.Builder<PortfolioDatabase>>()
 }
