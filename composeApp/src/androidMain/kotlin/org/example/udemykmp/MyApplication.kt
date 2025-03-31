2a87f08bf8153fd5cf868e1168fa87fe8426fbbc
Le Duy Anh <lenduya@gmail.com>
2025-03-31 15:41:35 +0200

    refactor (di): lift the modules out of redundant directory [kmp]

--- composeApp/src/androidMain/kotlin/org/example/udemykmp/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/org/example/udemykmp/MyApplication.kt
@@ -1,7 +1,6 @@
 package org.example.udemykmp
 
 import android.app.Application
-import org.example.udemykmp.di.initKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinComponent
