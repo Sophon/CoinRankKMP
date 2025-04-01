e1862e288c8112d22b4835e758704859ea2a6324
Le Duy Anh <lenduya@gmail.com>
2025-04-01 21:08:01 +0200

    feat (portfolio): portfolio part of the UI [kmp]

--- composeApp/src/commonMain/kotlin/org/example/udemykmp/features/portfolio/ui/PortfolioScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/features/portfolio/ui/PortfolioScreen.kt
@@ -1,6 +1,8 @@
 package org.example.udemykmp.features.portfolio.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,6 +15,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -22,8 +28,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
 import org.example.udemykmp.theme.localAppColorPalette
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -31,6 +40,7 @@ import org.koin.compose.viewmodel.koinViewModel
 fun PortfolioScreen(
     onCoinClick: (String) -> Unit,
     onBuyCoinClick: () -> Unit,
+    onDiscoverClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val vm = koinViewModel<PortfolioViewModel>()
@@ -52,6 +62,7 @@ fun PortfolioScreen(
             state = state,
             onCoinClick = onCoinClick,
             onBuyCoinClick = onBuyCoinClick,
+            onDiscoverClick = onDiscoverClick,
             modifier = modifier
         )
     }
@@ -62,6 +73,7 @@ private fun PortfolioScreenContent(
     state: PortfolioViewState,
     onCoinClick: (String) -> Unit,
     onBuyCoinClick: () -> Unit,
+    onDiscoverClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -76,7 +88,12 @@ private fun PortfolioScreenContent(
             isBuyButtonShown = state.isBuyButtonShown,
             onBuyButtonClick = onBuyCoinClick,
         )
-//        CoinsList()
+
+        PortfolioCoinsList(
+            coins = state.coins,
+            onCoinClick = onCoinClick,
+            onDiscoverClick = onDiscoverClick,
+        )
     }
 }
 
@@ -139,6 +156,140 @@ private fun Balance(
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun PortfolioCoinsList(
+    coins: List<UiPortfolioCoin>,
+    onCoinClick: (String) -> Unit,
+    onDiscoverClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
+    ) {
+        if (coins.isEmpty()) {
+            EmptyPortfolioSection(onDiscoverClick)
+            return@Box
+        } else {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    text = "💰 Owned Coins:", //TODO: strings
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+
+                Spacer(Modifier.height(8.dp))
+
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(coins) { coin ->
+                        PortfolioCoinListItem(
+                            coin = coin,
+                            onCoinClick = onCoinClick,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+//TODO: refactor to core/ui/gallery with the coinlist item variant
+@Composable
+private fun PortfolioCoinListItem(
+    coin: UiPortfolioCoin,
+    onCoinClick: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {onCoinClick(coin.id) }
+            .padding(16.dp)
+    ) {
+        AsyncImage(
+            model = coin.iconUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .padding(4.dp)
+                .clip(CircleShape)
+                .size(40.dp)
+        )
+        Spacer(Modifier.width(16.dp))
+        Column(
+            modifier = Modifier.weight(1f),
+        ) {
+            Text(
+                text = coin.name,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+            )
+            Spacer(Modifier.height(4.dp))
+            Text(
+                text = coin.amountInUnit,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+            )
+        }
+        Spacer(Modifier.width(16.dp))
+        Column(
+            horizontalAlignment = Alignment.End,
+        ) {
+            Text(
+                text = coin.amountInFiat,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(
+                text = coin.performancePercent,
+                color = if (coin.isPositivePerformance) localAppColorPalette.current.profitGreen else localAppColorPalette.current.lossRed,
+                fontSize = MaterialTheme.typography.titleSmall.fontSize,
+            )
+        }
+    }
+}
+
+@Composable
+private fun EmptyPortfolioSection(
+    onDiscoverClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "No coins in your portfolio yet.", //TODO: strings
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = MaterialTheme.typography.titleSmall.fontSize,
+        )
+        Button(
+            onClick = onDiscoverClick,
+            colors = ButtonDefaults.buttonColors(containerColor = localAppColorPalette.current.profitGreen),
+            contentPadding = PaddingValues(horizontal = 64.dp),
+        ) {
+            Text(
+                text = "Discover coins", //TODO: strings
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
         }
     }
 }
