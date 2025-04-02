c182510950a506e6f4e54c69fee1f1e4d506bbf3
Le Duy Anh <lenduya@gmail.com>
2025-04-03 01:37:58 +0200

    refactor (core): move the constants to the core package [kmp]

--- composeApp/src/commonMain/kotlin/org/example/udemykmp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/App.kt
@@ -2,6 +2,7 @@ package org.example.udemykmp
 
 import androidx.compose.runtime.Composable
 import org.example.udemykmp.features.coins.ui.CoinsListScreen
+import org.example.udemykmp.features.portfolio.ui.PortfolioScreen
 import org.example.udemykmp.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -9,6 +10,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     AppTheme {
-        CoinsListScreen {  }
+//        CoinsListScreen {}
+        PortfolioScreen({}, {}, {})
     }
 }
--- composeApp/src/commonMain/kotlin/org/example/udemykmp/features/coins/data/remote/impl/CoinsKtorRemoteDataSource.kt
+++ composeApp/src/commonMain/kotlin/org/example/udemykmp/features/coins/data/remote/impl/CoinsKtorRemoteDataSource.kt
@@ -2,7 +2,7 @@ package org.example.udemykmp.features.coins.data.remote.impl
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import org.example.udemykmp.features.coins.data.remote.BASE_URL
+import org.example.udemykmp.core.data.remote.BASE_URL
 import org.example.udemykmp.features.coins.data.remote.dto.CoinDetailsResponseDto
 import org.example.udemykmp.features.coins.data.remote.dto.CoinPriceHistoryResponseDto
 import org.example.udemykmp.features.coins.data.remote.dto.CoinsListResponseDto
